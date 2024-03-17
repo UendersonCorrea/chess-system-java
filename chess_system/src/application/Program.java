bf53db8a4e21db6cbfd3d84b03621a5365a7d86c
UendersonCorrea <uenderson.oliveira@gmail.com>
2024-03-17 17:54:45 -0300

    191 possible move of a piece

--- chess_system/src/application/Program.java
+++ chess_system/src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import boardgame.Board;
+import boardgame.BoardException;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -11,26 +14,40 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Board board = new Board(8,8);
+
+		Board board = new Board(8, 8);
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		while(true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.print("Target: ");
-			ChessPosition target =UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		while (true) {
+			try {
+
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (BoardException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} 
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+
 		}
-		
-		
-		
-		
+
 	}
 
 }
