9ee346b94dd18e23dee8546ebb03b77aeb89bb9b
UendersonCorrea <uenderson.oliveira@gmail.com>
2024-03-17 17:14:23 -0300

    189-moving pieces

--- chess_system/src/application/Program.java
+++ chess_system/src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import java.util.Scanner;
+
 import boardgame.Board;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
@@ -9,10 +13,22 @@ public class Program {
 		// TODO Auto-generated method stub
 		
 		Board board = new Board(8,8);
-		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.print("Target: ");
+			ChessPosition target =UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 		
-		UI.printBoard(chessMatch.getPieces());
+		
 		
 		
 	}
--- chess_system/src/boardgame/Board.java
+++ chess_system/src/boardgame/Board.java
@@ -24,9 +24,9 @@ public class Board {
 	}
 
 	public Piece piece(int row, int column) {
-		 if (!positionExists(row,column)) {
-		 throw new BoardException("Position not on the board");
-		 }
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 
@@ -46,6 +46,19 @@ public class Board {
 		piece.position = position;
 	}
 
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position)==null) {
+			return null;
+		}
+		Piece aux =piece(position);
+		aux.position =null;
+		pieces[position.getColumn()][position.getColumn()]=null;
+		return aux;
+	}
+
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -55,9 +68,10 @@ public class Board {
 	}
 
 	public boolean thereIsAPiece(Position position) {
-		 if (!positionExists(position)) {
-		 throw new BoardException("Position not on the board");
-		 }
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return piece(position) != null;
 	}
+
 }
